48cf49cddfa8448008706cdf3caf19bf3cd75c7e
sergiomelquiades <sergioalmeida.2011@gmail.com>
2018-08-08 13:52:27 -0300

    Aula 1:Refatoração do Exercicio 2 da lista 1

--- src/CaculadorRetangulo.java
+++ src/CaculadorRetangulo.java
@@ -2,28 +2,38 @@ public class CaculadorRetangulo {
 	//Atributos
 	private double comprimento;
 	private double largura;
+	private String descricao;
 
 	//Construtor
-	public CaculadorRetangulo(double comprimento, double largura){
+	public CaculadorRetangulo(double comprimento, double largura) {
 		this.comprimento = comprimento;
 		this.largura = largura;
 	}
 
 	//Metodos getters e setters
-	public void setComprimento(double comprimento){
+	public void setComprimento(double comprimento) {
 		this.comprimento = comprimento;
 	}
 
-	public void setLargura(double largura){
+	public void setLargura(double largura) {
 		this.largura = largura;
 	}
 
+	public String getDescricao() {
+		return descricao;
+	}
+
+	public void setDescricao(String descricao) {
+		this.descricao = descricao;
+	}
+
+
 	//Metodos
-	public double calculoArea(){
+	public double calculoArea() {
 		return comprimento * largura;
 	}
 
-	public double calculoPerimetro(){
+	public double calculoPerimetro() {
 		return (2 * comprimento) + (2 * largura);
 	}
 }
--- test/CalculadorRetanguloTests.java
+++ test/CalculadorRetanguloTests.java
@@ -1,10 +1,24 @@
 public class CalculadorRetanguloTests {
 	public static void main(String[] args) {
-		CaculadorRetangulo calc = new CaculadorRetangulo(140, 200);
-		calc.calculoArea();
-		calc.calculoPerimetro();
-		System.out.println("A area é " + calc.calculoArea());
-		System.out.println("O perimetro é " + calc.calculoPerimetro());
-		//Verificar como fazer caso queira alterar os valores
+		CaculadorRetangulo calc = new CaculadorRetangulo(100, 200);
+		calc.setDescricao("Retangulo 1");
+		print(calc);
+
+
+		calc.setDescricao("Retangulo 2");
+		calc.setComprimento(200);
+		calc.setLargura(300);
+		print(calc);
+
+
+		calc.setDescricao("Retangulo 3");
+		calc.setComprimento(300);
+		calc.setLargura(400);
+		print(calc);
+
+	}
+
+	public static void print(CaculadorRetangulo caculadorRetangulo){
+		System.out.println("A area do " + caculadorRetangulo.getDescricao() + " é " + caculadorRetangulo.calculoArea() + " e o perimetro é " + caculadorRetangulo.calculoPerimetro());
 	}
 }
