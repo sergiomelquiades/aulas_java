d1d4fc9502430a70ebcb0db978ff46608ea26074
sergiomelquiades <sergioalmeida.2011@gmail.com>
2018-08-09 13:42:01 -0300

    Aula 1:Refatoração do Exercicio 2 da lista 1

--- src/MediaAluno.java
+++ src/MediaAluno.java
@@ -1,4 +1,46 @@
-package PACKAGE_NAME;
-
 public class MediaAluno {
+	//Atributos
+	private double nota1;
+	private double nota2;
+	private double nota3;
+	private double nota4;
+	private String nome;
+
+	//Construtor
+	public MediaAluno(double nota1, double nota2, double nota3, double nota4){
+		this.nota1 = nota1;
+		this.nota2 = nota2;
+		this.nota3 = nota3;
+		this.nota4 = nota4;
+	}
+
+	//Getters e Setters
+	public void setNota1(double nota1){
+		this.nota1 = nota1;
+	}
+
+	public void setNota2(double nota2){
+		this.nota2 = nota2;
+	}
+
+	public void setNota3(double nota3){
+		this.nota3 = nota3;
+	}
+
+	public void setNota4(double nota4){
+		this.nota4 = nota4;
+	}
+
+	public void setNome(String nome){
+		this.nome = nome;
+	}
+
+	public String getNome(){
+		return nome;
+	}
+
+	//Metodo
+	public double calculoMedia(){
+		return (nota1 + nota2 + nota3 + nota4) / 4;
+	}
 }
--- test/MediaAlunoTests.java
+++ test/MediaAlunoTests.java
@@ -1,4 +1,33 @@
-package PACKAGE_NAME;
-
 public class MediaAlunoTests {
+	public static void main(String[] args) {
+		MediaAluno calc = new MediaAluno(10, 8.5, 7.5, 8);
+		calc.setNome("Sergio");
+		print(calc);
+
+		calc.setNome("Eliane");
+		calc.setNota1(10);
+		calc.setNota2(9);
+		calc.setNota3(9.5);
+		calc.setNota4(8);
+		print(calc);
+
+		calc.setNome("Maryanne");
+		calc.setNota1(10);
+		calc.setNota2(9.4);
+		calc.setNota3(10);
+		calc.setNota4(8.5);
+		print(calc);
+
+		calc.setNome("Henrique");
+		calc.setNota1(10);
+		calc.setNota2(9.9);
+		calc.setNota3(8.9);
+		calc.setNota4(9);
+		print(calc);
+
+	}
+
+	public static void print(MediaAluno mediaAluno){
+		System.out.println("A média do aluno " + mediaAluno.getNome() + " é " + mediaAluno.calculoMedia());
+	}
 }
